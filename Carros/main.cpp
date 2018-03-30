4e0b7ee157b16d07d5fe812782561bded57af9cb
djalma <djalminhajr_cunha@hotmail.com>
2018-03-30 12:31:00 -0300

    mundo que tinha sido desenhado anteriormente agora foi gerado

--- Carros/main.cpp
+++ Carros/main.cpp
@@ -6,10 +6,9 @@
 #include "Carro.h"
 using namespace std;
 int main() {
-    int a,b,c;
-    cin>>a;
-    cin>>b;
-    c = a + b;
-    cout << "Hello, World! " << c <<endl;
+Mundo a;
+            cout<<a.geraMundo()<<" "<<endl;
+
+
     return 0;
 }
